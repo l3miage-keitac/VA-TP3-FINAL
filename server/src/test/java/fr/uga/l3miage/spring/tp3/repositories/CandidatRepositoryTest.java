158fba98551e5657e1ba71c6c01be575863e96f3
l3miage-couliban <n-djo-soro.coulibaly@etu.univ-grenoble-alpes.fr>
2024-04-05 16:56:28 +0200

    test: find all by test center entity code

--- server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidatRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidatRepositoryTest.java
@@ -1,7 +1,9 @@
 package fr.uga.l3miage.spring.tp3.repositories;
 
+import fr.uga.l3miage.spring.tp3.enums.TestCenterCode;
 import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
 import fr.uga.l3miage.spring.tp3.models.CandidateEvaluationGridEntity;
+import fr.uga.l3miage.spring.tp3.models.TestCenterEntity;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,8 +22,64 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CandidatRepositoryTest {
     @Autowired
     private CandidateRepository candidateRepository;
+
+    @Autowired
+    private TestCenterRepository testCenterRepository;
+
     @Autowired
     private CandidateEvaluationGridRepository gridRepository;
+
+    @Test
+    void testFindAllByTestCenterEntityCode() {
+        //given
+        TestCenterEntity testCenter = TestCenterEntity
+                .builder()
+                .code(TestCenterCode.GRE)
+                .city("Grenobe")
+                .university("UGA")
+                .build();
+
+        CandidateEntity cand1 = CandidateEntity
+                .builder()
+                .email("cand1@gmail.com")
+                .firstname("John")
+                .lastname("Doe")
+                .birthDate(LocalDate.parse("2007-01-08"))
+                .phoneNumber("07456798")
+                .testCenterEntity(testCenter)
+                .build();
+
+        CandidateEntity cand2 = CandidateEntity
+                .builder()
+                .email("cand2@gmail.com")
+                .firstname("Calvin")
+                .lastname("Klein")
+                .birthDate(LocalDate.parse("2005-05-08"))
+                .phoneNumber("06435676")
+                .testCenterEntity(testCenter)
+                .build();
+
+        Set<CandidateEntity> candidates = new HashSet<>();
+        candidates.add(cand1);
+        candidates.add(cand2);
+
+        testCenter.setCandidateEntities(candidates);
+
+        testCenterRepository.save(testCenter);
+
+
+        candidateRepository.save(cand1);
+        candidateRepository.save(cand2);
+
+        //when
+        Set<CandidateEntity> candidateEntities = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.GRE);
+
+        //then
+        assertThat(candidateEntities).hasSize(2);
+        assertThat(candidateEntities.stream().findFirst().get().getBirthDate()).isEqualTo(LocalDate.parse("2007-01-08"));
+    }
+
+
     @Test
     void findAllByCandidateEvaluationGridEntitiesGradeLessThan(){
         CandidateEvaluationGridEntity candidateEvaluationGrid1 = CandidateEvaluationGridEntity
