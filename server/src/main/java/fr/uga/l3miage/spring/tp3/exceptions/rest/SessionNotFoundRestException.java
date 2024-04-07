c5ad9297e06b843d503edbb72993d50fbdfa2bc5
l3miage-keitac <cire.keita@etu.univ-grenoble-alpes.fr>
2024-04-07 23:35:28 +0200

    Switch  from EVAL_STARTED to EVAL_ENDED status

---
+++ server/src/main/java/fr/uga/l3miage/spring/tp3/exceptions/rest/SessionNotFoundRestException.java
@@ -0,0 +1,12 @@
+package fr.uga.l3miage.spring.tp3.exceptions.rest;
+
+import lombok.Getter;
+
+@Getter
+public class SessionNotFoundRestException extends RuntimeException{
+    private final Long sessionId;
+    public SessionNotFoundRestException(String message, Long sessionId){
+        super(message);
+        this.sessionId = sessionId;
+    }
+}
--- server/src/main/java/fr/uga/l3miage/spring/tp3/services/SessionService.java
+++ server/src/main/java/fr/uga/l3miage/spring/tp3/services/SessionService.java
@@ -4,17 +4,18 @@ import fr.uga.l3miage.spring.tp3.components.ExamComponent;
 import fr.uga.l3miage.spring.tp3.components.SessionComponent;
 import fr.uga.l3miage.spring.tp3.enums.SessionStatus;
 import fr.uga.l3miage.spring.tp3.exceptions.rest.CreationSessionRestException;
+import fr.uga.l3miage.spring.tp3.exceptions.rest.SessionNotFoundRestException;
 import fr.uga.l3miage.spring.tp3.exceptions.technical.ExamNotFoundException;
+import fr.uga.l3miage.spring.tp3.exceptions.technical.SessionNotFoundException;
 import fr.uga.l3miage.spring.tp3.mappers.SessionMapper;
-import fr.uga.l3miage.spring.tp3.models.EcosSessionEntity;
-import fr.uga.l3miage.spring.tp3.models.EcosSessionProgrammationEntity;
-import fr.uga.l3miage.spring.tp3.models.EcosSessionProgrammationStepEntity;
-import fr.uga.l3miage.spring.tp3.models.ExamEntity;
+import fr.uga.l3miage.spring.tp3.models.*;
 import fr.uga.l3miage.spring.tp3.request.SessionCreationRequest;
 import fr.uga.l3miage.spring.tp3.responses.SessionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -46,6 +47,30 @@ public class SessionService {
             return sessionMapper.toResponse(sessionComponent.createSession(ecosSessionEntity));
         }catch (RuntimeException | ExamNotFoundException e){
             throw new CreationSessionRestException(e.getMessage());
+        }
+    }
+    // On supposera ici que la session dont l'identifiant est passé en paramètre a le status EVAL_STARTED
+    // Le passage de l'état EVAL_STARTED à EVAL_ENDED n'est possible que si la première étape est correcte
+    public SessionResponse updateSessionStatus(Long idSession) {
+        try {
+            EcosSessionEntity ecosSessionEntity =sessionComponent.getSessionById(idSession);
+            LocalDateTime latestDateTime = ecosSessionEntity
+                    .getEcosSessionProgrammationEntity()
+                    .getEcosSessionProgrammationStepEntities()
+                    .stream()
+                    .max(Comparator.comparing(EcosSessionProgrammationStepEntity::getDateTime))
+                    .orElse(null).getDateTime();
+            if((latestDateTime != null)){
+                if(ecosSessionEntity.getStatus().equals(SessionStatus.EVAL_STARTED)) {
+                    ecosSessionEntity.setStatus(SessionStatus.EVAL_ENDED);
+                }
+                else {
+                    throw new SessionNotFoundRestException(("L'étape précédente n'était pas EVAL_STARTED pour la session " + idSession), idSession);
+                }
+            }
+            return sessionMapper.toResponse(sessionComponent.getEcosSessionRepository().save(ecosSessionEntity));
+        }catch (SessionNotFoundException e){
+            throw new SessionNotFoundRestException(e.getMessage(), e.getSessionId());
         }
     }
 }
